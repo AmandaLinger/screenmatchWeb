aa45e3433a58241167ce96ffa545736831242808
Amanda Marques <amandamarqueslinger@gmail.com>
2026-06-11 15:44:29 -0300

    recebendo informações pelo html

--- src/main/java/br/com/estudos/screenmatchWeb/controller/FilmeController.java
+++ src/main/java/br/com/estudos/screenmatchWeb/controller/FilmeController.java
@@ -1,7 +1,9 @@
 package br.com.estudos.screenmatchWeb.controller;
 
+import br.com.estudos.screenmatchWeb.domain.filme.DadosCadastroFilme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +13,11 @@ public class FilmeController {
     @GetMapping //informa que ao realizar um get na página, essa função seja executada
     public String carregaPaginaFormulario(){
         return "filmes/formulario"; //chama a página html nesse caminho
+    }
+
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados){
+        System.out.println(dados);
+        return "filmes/formulario";
     }
 }
---
+++ src/main/java/br/com/estudos/screenmatchWeb/domain/filme/DadosCadastroFilme.java
@@ -0,0 +1,3 @@
+package br.com.estudos.screenmatchWeb.domain.filme;
+
+public record DadosCadastroFilme(String nome, Integer duracao, Integer ano, String genero) {}
