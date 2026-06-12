6f5c72438de724b843134a92fc3d817774ee7708
Amanda Marques <amandamarqueslinger@gmail.com>
2026-06-12 13:30:51 -0300

    implementando o método de alteração

--- src/main/java/br/com/estudos/screenmatchWeb/controller/FilmeController.java
+++ src/main/java/br/com/estudos/screenmatchWeb/controller/FilmeController.java
@@ -22,7 +22,11 @@ public class FilmeController {
     private FilmeRepository repository;
 
     @GetMapping("/formulario") //informa que ao realizar um get na página, essa função seja executada
-    public String carregaPaginaFormulario(){
+    public String carregaPaginaFormulario(Long id, Model model) {
+        if(id!=null){
+            var filme = repository.getReferenceById(id);
+            model.addAttribute("filme", filme);
+        }
         return "filmes/formulario"; //chama a página html nesse caminho
     }
 
